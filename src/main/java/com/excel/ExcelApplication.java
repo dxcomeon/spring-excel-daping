40ddb8ac0aac402ebb97a4e75594a220d939be2b
duxin <duxin@litsoft.com.cn>
2021-09-09 10:30:12 +0800

    jar

--- src/main/java/com/excel/ExcelApplication.java
+++ src/main/java/com/excel/ExcelApplication.java
@@ -4,20 +4,18 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-public class ExcelApplication extends SpringBootServletInitializer {
+public class ExcelApplication  {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(ExcelApplication.class, args);
 
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ExcelApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(ExcelApplication.class);
+//    }
 
 }
