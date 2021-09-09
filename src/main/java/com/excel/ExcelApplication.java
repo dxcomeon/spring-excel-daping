5ba176326f5937966156d894dad84f483c6ddcc0
duxin <duxin@litsoft.com.cn>
2021-09-09 18:37:25 +0800

    打包

--- src/main/java/com/excel/ExcelApplication.java
+++ src/main/java/com/excel/ExcelApplication.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-public class ExcelApplication  {
+public class ExcelApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(ExcelApplication.class, args);
 
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(ExcelApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExcelApplication.class);
+    }
 
 }
